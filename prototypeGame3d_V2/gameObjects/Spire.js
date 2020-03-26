d246cd124e896980940e91f4db1f6237b758e10d
PadnaVs <60653274+PadnaVs@users.noreply.github.com>
2020-03-26 16:16:40 +0300

    Update Spire.js

--- prototypeGame3d_V2/gameObjects/Spire.js
+++ prototypeGame3d_V2/gameObjects/Spire.js
@@ -14,7 +14,7 @@
 		
 		this._rotation = 0;
 		this.step  = 0;
-		this.speed = 0.265;
+		this.speed = 0.268;
 		
 ///////////////////////////////////////////////////////////////////////////
 		this.model = this.createSipe();
